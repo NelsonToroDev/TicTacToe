37cc0b1ab907b2a0f2e16a852132a6d195839842
Nelson Toro <nelson.toro.flores@gmail.com>
2024-01-18 15:13:54 -0400

    remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,51 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import confetti from 'canvas-confetti'
 import './App.css'
 import { Square } from './components/Square'
-import { TURNS } from "./constants.js";
-import { checkWinnerFrom, checkEndGame } from "./logic/board.js";
-import { WinnerModal } from "./components/WinnerModal";
-import { saveGameToStorage, resetGameToStorage } from "./storage/index.js"
+import { TURNS } from './constants.js'
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
+import { WinnerModal } from './components/WinnerModal'
+import { saveGameToStorage, resetGameToStorage } from './storage/index.js'
 
-function App () {
+function App() {
   console.log('render')
   const [board, setBoard] = useState(() => {
     console.log('Initializing board state')
     // Will resume the game if the page was refreshed from browser
-    const boardFromStorage = window.localStorage.getItem('board'); // Sync call and slow
-    return boardFromStorage
-      ? JSON.parse(boardFromStorage)
-      : Array(9).fill(null)
-  });
-  const [turn, setTurn] = useState(() =>
-    {
-      console.log('Initializing turn state')
-      // Will resume the game if the page was refreshed from browser
-      const turnFromStorage = window.localStorage.getItem('turn'); // Sync call and slow
-      return turnFromStorage ?? TURNS.X
-    }
-  );
+    const boardFromStorage = window.localStorage.getItem('board') // Sync call and slow
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    console.log('Initializing turn state')
+    // Will resume the game if the page was refreshed from browser
+    const turnFromStorage = window.localStorage.getItem('turn') // Sync call and slow
+    return turnFromStorage ?? TURNS.X
+  })
   // null there is no winner, false when exists a tie otherwise true
-  const [winner, setWinner] = useState(null);
+  const [winner, setWinner] = useState(null)
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
-    resetGameToStorage();
+    resetGameToStorage()
   }
- 
+
   const updateBoard = (index) => {
     // Avoid updated an already filled cell
     if (board[index] || winner) return
 
-    // Update the board 
+    // Update the board
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard) // async call
 
     // update turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-    setTurn(newTurn);
+    setTurn(newTurn)
 
     //save board
-    saveGameToStorage({ board: newBoard, turn: newTurn });
+    saveGameToStorage({ board: newBoard, turn: newTurn })
 
     // check winner
     const newWinner = checkWinnerFrom(newBoard)
@@ -66,30 +62,23 @@ function App () {
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reset Game</button>
       <section className='game'>
-        {
-          board.map((square, index) => {
-            return (
-              <Square
-                key={index}
-                index={index}
-                updateBoard={updateBoard}>
-                {square}
-              </Square>
-            )
-          })
-        }
+        {board.map((square, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {square}
+            </Square>
+          )
+        })}
       </section>
 
       <section className='turn'>
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
-      
+
       <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
   )
 }
-
-
 
 export default App
