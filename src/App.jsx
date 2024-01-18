71773bd6ac1908be1cfe090d2b5f4e944a72a3e3
Nelson Toro <nelson.toro.flores@gmail.com>
2024-01-17 21:14:44 -0400

    Support to save board on localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,23 @@ import { WinnerModal } from "./components/WinnerModal";
 
 
 function App () {
-  
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  console.log('render')
+  const [board, setBoard] = useState(() => {
+    console.log('Initializing board state')
+    // Will resume the game if the page was refreshed from browser
+    const boardFromStorage = window.localStorage.getItem('board'); // Sync call and slow
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null)
+  });
+  const [turn, setTurn] = useState(() =>
+    {
+      console.log('Initializing turn state')
+      // Will resume the game if the page was refreshed from browser
+      const turnFromStorage = window.localStorage.getItem('turn'); // Sync call and slow
+      return turnFromStorage ?? TURNS.X
+    }
+  );
   // null there is no winner, false when exists a tie otherwise true
   const [winner, setWinner] = useState(null);
 
@@ -19,6 +33,8 @@ function App () {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
  
   const updateBoard = (index) => {
@@ -33,6 +49,10 @@ function App () {
     // update turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn);
+
+    //save board
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
 
     // check winner
     const newWinner = checkWinnerFrom(newBoard)
